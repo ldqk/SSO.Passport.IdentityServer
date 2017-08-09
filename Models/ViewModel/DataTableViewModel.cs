acf5c2e1be4876c165b37c2cfcaf0afee4d7aee5
ldqk <1170397736@qq.com>
2017-08-09 19:53:19 +0800

    update

---
+++ Models/ViewModel/DataTableViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.ViewModel
+{
+    public class DataTableViewModel
+    {
+        public dynamic data { get; set; }
+        public int recordsFiltered { get; set; }
+        public int recordsTotal { get; set; }
+
+    }
+}
---
+++ Models/ViewModel/PageDataViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Models.ViewModel
+{
+    public class PageDataViewModel
+    {
+        public object Data { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPage { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
