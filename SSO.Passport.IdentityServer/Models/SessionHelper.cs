09123780afee5efe78d98ae3161320e0798d7590
ldqk <1170397736@qq.com>
2017-08-14 19:32:29 +0800

    Add

---
+++ SSO.Passport.IdentityServer/Models/SessionHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Dynamic;
+using Masuit.Tools.NoSQL;
+
+namespace SSO.Passport.IdentityServer.Models
+{
+    public static class SessionHelper
+    {
+        public static RedisHelper RedisHelper { get; set; } = new RedisHelper();
+
+        public static void Set(string key, object value)
+        {
+            RedisHelper.SetString(key, value, TimeSpan.FromMinutes(20));
+        }
+
+        public static T Get<T>(string key)
+        {
+            RedisHelper.Expire(key, TimeSpan.FromMinutes(20));
+            return RedisHelper.GetString<T>(key);
+        }
+    }
+}
